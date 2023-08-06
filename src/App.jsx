16ea377b65237795dc6aa6d6eae58b6eb7e302d6
Michael Loughman <mcloughman@gmail.com>
2023-08-05 22:49:51 -0700

    Add skeleton code for Home and Question components. Handle the decoding and shuffling of answers within the useEffect hook in which we also setQuestions to be an object with a question, shuffledAnswers, and correctAnswer props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react"
+import he from "he"
+import { nanoid } from "nanoid"
 
+import Home from "./components/Home"
+import Question from "./components/Question"
 const App = () => {
   const [questions, setQuestions] = useState([])
+  const [shuffledAnswers, setShuffledAnswers] = useState([])
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
@@ -12,7 +17,23 @@ const App = () => {
         }
         const data = await response.json()
         console.log(data.results)
-        setQuestions(data.results)
+        // Process the fetched data to create an array of questions with shuffled answers
+        const processedQuestions = data.results.map((question) => {
+          const decodedQuestion = question.question
+          const decodedCorrectAnswer = he.decode(question.correct_answer)
+          const decodedWrongAnswers = question.incorrect_answers.map((answer) =>
+            he.decode(answer)
+          )
+
+          const allAnswers = [...decodedWrongAnswers, decodedCorrectAnswer]
+          const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5)
+          return {
+            question: decodedQuestion,
+            shuffledAnswers,
+            correctAnswer: decodedCorrectAnswer,
+          }
+        })
+        setQuestions(processedQuestions)
       } catch (e) {
         console.log("Error", e.message)
         return
@@ -20,6 +41,17 @@ const App = () => {
     }
     fetchQuestions()
   }, [])
+
+  const quizQuestions = questions.map((question, index) => (
+    <Question key={index} question={question} />
+  ))
+
+  return (
+    <>
+      <Home />
+      {quizQuestions}
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Question.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react"
+import he from "he"
+import { nanoid } from "nanoid"
+
+const Question = (props) => {
+  return <h1>Question</h1>
+}
+
+export default Question
